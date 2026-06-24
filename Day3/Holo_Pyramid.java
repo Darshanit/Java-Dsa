29e6affa799790ba44365e9aec8c23798a4c055b
Darshan Sivakumar <157400637+Darshanit@users.noreply.github.com>
2026-06-24 15:00:26 +0530

    Add files via upload

---
+++ Day3/Holo_Pyramid.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars and inner spaces
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (k == 1 || k == 2 * i - 1 || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
